cad2404c9c93db7e5d3f3b87cccd75eeae20b007
LYMDS <609586869@qq.com>
2019-04-27 19:44:15 +0800

    小改动

--- ols_project/olsapp/views.py
+++ ols_project/olsapp/views.py
@@ -278,7 +278,7 @@ def garage_msg(request):
     park_msg = Garage_parking_state_table.objects.get(garage_num = garage)
     ready_load = []
     for i in park_msg：
-        load = [i.parking_num, i.exist_car, i.charge_state, i.lock_state, i.car_num]
+        load = [i.parking_num, i.exist_car, i.charge_state, i.lock_state]
         ready_load.append(load)
     ready_load = sorted(ready_load,key=itemgetter(0))
     m = 0
