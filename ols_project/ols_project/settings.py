e57d8a4a733b3365653b60f00f842a7ffba21885
LYMDS <609586869@qq.com>
2019-06-01 11:49:45 +0800

    配置了redis数据库

--- ols_project/ols_project/settings.py
+++ ols_project/ols_project/settings.py
@@ -121,6 +121,19 @@ USE_L10N = True
 
 USE_TZ = False
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+REDIS_TIMEOUT=7*24*60*60
+CUBES_REDIS_TIMEOUT=60*60
+NEVER_REDIS_TIMEOUT=365*24*60*60
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
