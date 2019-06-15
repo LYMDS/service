2e642130410b0444e102b4caf7147e41f54be846
LYMDS <609586869@qq.com>
2019-06-15 20:55:41 +0800

    更新mqtt系列转发代码

--- mqtt/test.py
+++ mqtt/test.py
@@ -13,7 +13,7 @@ def send(topic,context):
                auth=U_P)
 
 #send("C/gds100001/P","i am your father")
-'''---------------------------------------------'''
+'''---------------------------------------------
 import connect
 topic = "C/gds100001/P"
 topic = topic.rstrip("P")+"S"
@@ -22,4 +22,18 @@ data = connect.sql_not_roll(SQL)
 print(data[0][1])
 print(type(data))
 print(len(data))
+'''
+import get
+url = "http://120.77.156.184/mqtt_receive"
+data = {
+    'garage': 1,
+    'garage_type' : 0,
+    'running_state' : 's',
+    'exist_car' : "010100101111",
+    'door_state' : "010",
+    'side_control' : "f",
+}
+response = requests.get(url,headers=headers,params=data)
+json = response.json()
+
 
