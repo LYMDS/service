cd323ab8786281a8c9a8d831ba8a2a56e9e19a65
LYMDS <609586869@qq.com>
2019-06-23 22:06:33 +0800

    在服务器上配置了站点管理

--- ols_project/olsapp/admin.py
+++ ols_project/olsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User_info_table
+@admin.register(User_info_table)
+class User_info_tableAdmin(admin.ModelAdmin):
+    pass
