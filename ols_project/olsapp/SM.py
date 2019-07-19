a01ff29a1859c4db441047ab1a177124b0d45bde
LYMDS <609586869@qq.com>
2019-07-20 00:26:37 +0800

    调试短信模块

--- ols_project/olsapp/SM.py
+++ ols_project/olsapp/SM.py
@@ -28,3 +28,7 @@ def send_Email(code):
     smtp.sendmail(sender,receiver,msg.as_string()) #发送邮件 ，这里有三个参数
     smtp.quit()
     return code
+if __name__ == "__main__":
+    code = "875930"
+    a = send_Email(code)
+    print(a)
